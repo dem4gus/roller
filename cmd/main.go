da0259628ff9ff2ce821a0562386d5f720c82c47
Jason Healy <jason@healy.app>
2026-01-30 22:17:40 -0500

    implement cli

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/dem4gus/roller"
 )
 
 func main() {
-	result, err := roller.Roll(2, 6, 0)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
+	if len(os.Args) == 1 {
+		for {
+			reader := bufio.NewReader(os.Stdin)
+			input, _ := reader.ReadString('\n')
+			result, err := roller.Roll(strings.TrimSpace(input))
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error: %s", err)
+				os.Exit(1)
+			}
+			fmt.Println(result)
+		}
 	}
-	fmt.Printf("Rolling 2d6+2: %d", result)
+
+	inputs := os.Args[1:]
+	for _, in := range inputs {
+		result, err := roller.Roll(in)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %s", err)
+			os.Exit(1)
+		}
+		fmt.Println(result)
+	}
 }
